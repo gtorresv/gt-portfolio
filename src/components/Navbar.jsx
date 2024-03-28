ae005f6289fc4670128811eb50286de5f6e4f703
gtorresv <giselle.torres15@gmail.com>
2024-03-27 22:27:58 -0400

    added Navbar and footer components and created files for pages

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-tabs">
+                
+            </div>
+            <h1>Giselle Torres-Villa</h1>
+            <div className="contact-tab">Contact</div>
+        </nav>
+    )
+}
+
+export default Navbar
