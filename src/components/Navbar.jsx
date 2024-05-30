ae3e72508f05a0eae2eca659c012f63c22382262
gtorresv <giselle.torres15@gmail.com>
2024-05-29 23:02:49 -0400

    added the navbar tabs as links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="navbar-tabs">
-                
-            </div>
+            <ul className="navbar-tabs">
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link to="/resume">Resume</Link>
+                </li>
+            </ul>
             <h1>Giselle Torres-Villa</h1>
-            <div className="contact-tab">Contact</div>
+            <ul className="navbar-tabs">
+                <li className="contact-tab">
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
         </nav>
     )
 }
