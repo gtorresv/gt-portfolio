2fe571f008aa220e3d8a6dca00b7dba276eaf0ef
gtorresv <giselle.torres15@gmail.com>
2024-06-02 00:24:29 -0400

    added about me intro and portfolio project cards

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,18 @@
 const About = () => {
-    return (
-        <div>
-        <h2>About Me</h2>
-        </div>
-    )
+  return (
+    <div className="about-section">
+      <h2>About Me</h2>
+      <h3>Bonjour!</h3>
+      <p>
+        I'm Giselle, a full stack web developer with a higher inclination on the
+        back-end. 
+        <br />
+        With my knowledge in web development and a special interest in
+        the medical field and education, I aspire to make a difference by
+        helping create more rewarding and beneficial projects.
+      </p>
+    </div>
+  )
 }
 
-export default About
+export default About;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,9 +1,59 @@
 const Portfolio = () => {
     return (
         <div>
-            <h2>Portfolio</h2>
+            <h2>Porfolio</h2>
+            <div className="projects">
+            <div className="card border-light" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div className="card border-light" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div className="card border-light" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div className="card border-light" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div className="card border-light" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            <div className="card border-light" style={{width: '18rem'}}>
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            </div>
         </div>
-    )
+      )
 }
 
 export default Portfolio
